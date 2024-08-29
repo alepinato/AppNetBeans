1044ceda2d2f3cd44d291c3e3bf4af7b3b034779
Alejandro Pinato <54086973+alepinato@users.noreply.github.com>
2024-08-29 16:21:45 -0300

    Add files via upload

---
+++ ProyFormMenu/src/Clases/Usuarios.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author alepi
+ */
+public class Usuarios {
+    private String nombre;
+    private String apellido;
+    private String username;
+    private String passwd;
+
+    public Usuarios() {
+        
+    }
+    
+    public Usuarios(String nombre, String apellido, String username, String passwd) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.username = username;
+        this.passwd = passwd;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+    
+}
